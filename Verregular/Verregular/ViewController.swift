9aa690976dfcbb3a2d9163603e909f5879f260fd
Марина <77566874+marizhura@users.noreply.github.com>
2023-08-07 15:48:15 +0300

    initial commit

---
+++ Verregular/Verregular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Verregular
+//
+//  Created by Марина Журавлева on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
